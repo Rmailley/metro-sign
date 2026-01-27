350675854da1b2f82812ad9f0580f9b2e2aaf4fb
Rmailley <72274883+Rmailley@users.noreply.github.com>
2026-01-27 08:34:25 -0500

    reverted paths

--- rpi-metro-display/run.sh
+++ rpi-metro-display/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /home/dietpi/metro-sign
+cd /root/metro-sign
 sleep 10  # Wait for network to be fully ready
 git pull --ff-only || true  # Don't fail if pull fails
-python3 /home/dietpi/metro-sign/rpi-metro-display/rpi-metro-display.py \
-  /home/dietpi/metro-sign/rpi-metro-display/log.txt \
+python3 /root/metro-sign/rpi-metro-display/rpi-metro-display.py \
+  /root/metro-sign/rpi-metro-display/log.txt \
   e28810b76b054f1d9b49eeb67548d077 \
   E04 \
   2 \
-  /home/dietpi/metro-sign/rpi-metro-display/fonts/6x10.bdf \
-  /home/dietpi/metro-sign/rpi-metro-display/lines.json \
-  /home/dietpi/metro-sign/rpi-metro-display/stations.json
+  /root/metro-sign/rpi-metro-display/fonts/6x10.bdf \
+  /root/metro-sign/rpi-metro-display/lines.json \
+  /root/metro-sign/rpi-metro-display/stations.json
